84a9e89368e3d101edae426f155091385f6057e7
ttikr <ia03.danchuk.volodymyr@gmail.com>
2024-09-04 15:25:41 +0300

    After refactoring names and methods of casting commit

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+
+        if (stringBuilder.isEmpty()) {
+            return new String[0];
+        }
+        String[] words = stringBuilder.toString().split("\\W");
+
+        ArrayList<String> wordsArray = new ArrayList<>();
+        for (String word : words) {
+            word = word.toLowerCase();
+            if (word.startsWith("w")) {
+                wordsArray.add(word);
+            }
+        }
+
+        String[] res = new String[wordsArray.size()];
+        res = wordsArray.toArray(res);
+        Arrays.sort(res);
+        return res;
     }
 }
